56c9fa98c28a67aef7f71e9268377db422738b99
SantaClaus25 <42184814+SantaClaus25@users.noreply.github.com>
2019-06-24 13:10:55 +0400

    Create 07.Flatten a List.py

---
+++ Home/07.Flatten a List.py
@@ -0,0 +1,19 @@
+def flat_list(array):
+    k = 0
+    while(k<10):
+        new = []
+        for i in array:
+            try:
+                new.extend(i)
+            except:
+                new.append(i)
+        k+=1
+        array = new
+    return new
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
