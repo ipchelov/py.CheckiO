f6fff1f47c93d686510b6e8b119983ca679f9395
SantaClaus25 <42184814+SantaClaus25@users.noreply.github.com>
2019-06-24 10:55:58 +0400

    Create 06.Sort Array by Element Frequency.py

---
+++ Home/06.Sort Array by Element Frequency.py
@@ -0,0 +1,20 @@
+def frequency_sort(items):
+    my_dict = {x:items.count(x) for x in items}
+    my_list = []
+    for k, v in sorted(my_dict.items(), key=lambda kv:kv[1],reverse=True):
+        for i in range(v):
+            my_list.append(k)
+    return my_list
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]))
+
+    # # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4])) == [4, 4, 4, 4, 6, 6, 2, 2]
+    assert list(frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob'])) == ['bob', 'bob', 'bob', 'carl', 'alex']
+    assert list(frequency_sort([17, 99, 42])) == [17, 99, 42]
+    assert list(frequency_sort([])) == []
+    assert list(frequency_sort([1])) == [1]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
