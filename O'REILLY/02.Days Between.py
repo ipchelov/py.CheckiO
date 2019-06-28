baa51497a0b47eb2170e53326817c9c8db32f1b5
SantaClaus25 <42184814+SantaClaus25@users.noreply.github.com>
2019-06-28 14:40:00 +0400

    Create 02.Days Between.py

---
+++ O'REILLY/02.Days Between.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+def days_diff(date1, date2):
+    a = list(date1)
+    b = list(date2)
+    return abs(date(a[0],a[1],a[2]) - date(b[0], b[1], b[2])).days
+
+if __name__ == '__main__':
+    print("Example:")
+    print(days_diff((1982, 4, 19), (1982, 4, 22)))
+
+    # # These "asserts" are used for self-checking and not for an auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+    print("Coding complete? Click 'Check' to earn cool rewards!")
